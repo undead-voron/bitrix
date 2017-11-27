78f5919ad7a6a918e58cb852bfacc9c5ce217638
voron <undeadvoron@gmail.com>
2017-11-27 04:40:59 +0000

    added some comments into server file

--- server.custom.js
+++ server.custom.js
@@ -15,6 +15,7 @@ var server = http.createServer(function(request, response) {
   var headers = {};
   for(var i in request.headers){
     console.log(i + ': ' + request.headers[i]);
+    // i think this is the list of all default headers the don't need to be maanged and may render the request inval
     if (i != "host" && i != "x-forwarded-proto" && i != "x-forwarded-port" && i != "x-region" && i != "x-forwarded-for" && i != "connection")
       headers[i] = request.headers[i];
   }
@@ -24,41 +25,13 @@ var server = http.createServer(function(request, response) {
     body.push(chunk);
   }).on('end', () => {
     body = Buffer.concat(body).toString();
+    // write request body into console
     console.log('req body', body);
-    // at this point, `body` has the entire request body stored in it as a string
   });
-  
-  //var httpRequest = function (params, postData) {
-  //  return new Promise(function(resolve, reject) {
-  //    var req = http.request(params, function(res) {
-  //      console.log('my statusCode=' + res.statusCode);
-  //      console.log('res=' + res);
-  //      // reject on bad status
-  //      if (res.statusCode < 200 || res.statusCode >= 300) {
-  //        return reject(new Error('statusCode=' + res.statusCode));
-  //      }
-  //    });
-  //    // IMPORTANT
-  //    req.end();
-  //  });
-  //}
-  //
-  //
-  //var params = {
-  //  host: 'php-bitrix-unix-voron.c9users.io',
-  //  //port: 4000,
-  //  method: request.method,
-  //  headers: request.headers
-  //};
-  // this is a get, so there's no post data
-  
-  //httpRequest(params).then(function(body) {
-  //  console.log('then: ',body);
-  //});
-  
   
   
   var newRequest = http.request({
+    // server to connect to
     hostname: "php-bitrix-unix-voron.c9users.io",
     path: request.url,
     method: request.method,
@@ -79,12 +52,6 @@ var server = http.createServer(function(request, response) {
     });
   newRequest.end();
   
-  console.log('1');
-  
-  //response.writeHead(200, {"Content-Type": "text/html"});
-  //var d = new Date();
-  //response.write("success\nPHPSESSID\n238a0ab1b66143826e13e5072ea6c1bc\nsessid=16f6f901e780ab7c526aed8b452e42b6\ntimestamp=" + d.getTime());
-  //response.end();
 });
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
